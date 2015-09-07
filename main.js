9f7feca3e9efc700be49ccc297a5c7e536dd345d
Alex Moore <truckfondue@gmail.com>
2015-09-07 12:04:50 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// 1. 0 100 200 300 400 500
+// var count = 0
+// for (var i = 0; i < 6; i++) {
+// 	console.log(count)
+// 	count += 100
+// };
+
+// 2. 1 2 4 8 16 32 64
+// var base = 1
+// for (var i = 0; i < 7; i++) {
+// 	console.log(base)
+// 	base *= 2
+// };
+
+// 3. 1 1 1 2 2 2 3 3 3
+// for (var i = 0; i < 3; i++) {
+// 	console.log(i + 1)	
+// 	console.log(i + 1)
+// 	console.log(i + 1)
+// };
+
+// 4. 3 6 9 12 15
+// var multipleOfThree = 0
+// for (var i = 0; i < 5; i++) {
+// 	multipleOfThree += 3
+// 	console.log(multipleOfThree)
+// };
+
+// 4. 9 8 7 6 5 4 3 2 1 0
+// for (var i = 10; i ; i--) {
+// 	console.log(i - 1)
+// };
+
+// 5. 0 1 2 3 0 1 2 3 0 1 2 3
+// for (var i = 0; i < 3; i++) {
+// 	for (var j = 0; j < 4; j++) {
+// 	console.log(j)
+// 	};
+// };
